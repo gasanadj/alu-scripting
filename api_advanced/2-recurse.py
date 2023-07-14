e4864cb6c978e0907044f808f2a8015e4b5c7a12
gasanadj <122342575+gasanadj@users.noreply.github.com>
2023-07-14 14:54:47 +0200

    Update 2-recurse.py

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-""""Recurse"""
+""" This module uses recursion to get hot articles"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """"
-    Reddit sends an after property in the response.
-    Keep retrieving comments until after is null.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    response = requests.get(url, headers=header, params=param)
-
-    if response.status_code != 200:
-        return None
+def recurse(subreddit, hot_list=[], after=None):
+    BASE_URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Didas Junior'}
+    params = {'after': after}
+    response = requests.get(BASE_URL, headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data is not None:
+            children = data.get('children')
+            if children is not None:
+                for child in children:
+                    hot_list.append(child.get('data').get('title'))
+                after = data.get('after')
+                if after is not None:
+                    return recurse(subreddit, hot_list, after)
+                else:
+                    return hot_list
+        else:
+            return hot_list
     else:
-        json_res = response.json()
-        # print(json_res.get('data').get('after'))
-        after = json_res.get('data').get('after')
-        has_next = \
-            json_res.get('data').get('after') is not None
-        # print(has_next)
-        hot_articles = json_res.get('data').get('children')
-        [hot_list.append(article.get('data').get('title'))
-         for article in hot_articles]
-        # print(len(hot_list))
-        # print(hot_list)
-        return recurse(subreddit, hot_list, after=after) \
-            if has_next else hot_list
+        return None
